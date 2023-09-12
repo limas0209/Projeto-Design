d9107b97eb3dfdd818a9f20e072c727781d6e63b
Henrique <riquelima00@gmail.com>
2023-09-12 12:01:56 -0300

    Projeto-Design

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.scss";
+
+function App() {
+ 
+  return (
+    <>
+      <div className="container">
+        <seciton className="estrutura">
+          <article className="box">
+            <h1>Nossa estrutura</h1>
+            <img src="https://fastly.picsum.photos/id/664/350/200.jpg?hmac=C21F3kpMK0C7vb25ZRiytVFPV3OcC1_v6D6uefLOWZA" alt=""></img>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <a href="#">
+              Veja Mais
+            </a>
+          </article>
+
+          <article className="box">
+            <h1>Nossa estrutura</h1>
+            <img src="https://fastly.picsum.photos/id/982/350/200.jpg?hmac=6Ex6UvKy1c6zuKCPdReJJQWhHf7s1HmdggKkqcU54Ik" alt=""></img>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <a href="#">
+              Veja Mais
+            </a>
+          </article>
+        </seciton>       
+      </div>
+    </>
+  )
+}
+
+export default App
